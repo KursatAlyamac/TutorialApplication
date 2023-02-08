f3a03703f9de0c9761257d8ddd1ab1809dfb9dfa
Egehan <egehanyldz7@gmail.com>
2023-02-08 09:35:07 +0300

    min max added

--- src/App.java
+++ src/App.java
@@ -59,4 +59,22 @@ public class App {
         System.out.println("Sum of elements with odd indexes: " + oddSum);
         System.out.println("Sum of elements with even indexes: " + evenSum);
     }
+    public int findMax(int[] arr){
+        int max = arr[0];
+        for(int i = 1; i<arr.length;i++){
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+    public int findMin(int[] arr){
+        int min = arr[0];
+        for(int i = 1; i<arr.length;i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+        }
+        return min;
+    }
 }
